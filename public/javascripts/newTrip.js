a484765eac2da12822dcd035b41025e01feee6f3
Rizzo98 <andrearizzardi98@gmail.com>
2018-09-28 15:06:41 +0200

    Salvataggio immagini in localStorage. Problema quando clicchi su map durante new marker

--- public/javascripts/newTrip.js
+++ public/javascripts/newTrip.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   localStorage.clear();
+  localStorage.setItem('IdCounter',0)
 
   $('#newMarker').click(()=>{
     $('#newTripModal').modal('toggle')
@@ -9,12 +10,30 @@ $(document).ready(function(){
   })
 
   $('#confirmMarker').click(()=>{
+    id = localStorage.IdCounter
+    images = [$('#img1').attr('src'),$('#img2').attr('src'),$('#img3').attr('src'),$('#img4').attr('src')]
+    localStorage.setItem(id, JSON.stringify(images))
+    localStorage.IdCounter ++
     $('#newTripModal').modal('show')
-    id = (parseFloat($('#newMarkerModal').attr('lat')).toFixed(4)+parseFloat($('#newMarkerModal').attr('lng')).toFixed(4)+$('#newMarkerName').val().length)
-    localStorage.setItem(id ,[$('#img1').attr('src'),$('#img2').attr('src'),$('#img3').attr('src'),$('#img4').attr('src')])
     $("#markerList").append('<li class="addMarkerli" lat ='
                             + $('#newMarkerModal').attr('lat') +' lng = '+ $('#newMarkerModal').attr('lng') +' description = "' + $("#newMarkerDescription").val()
                             +'" name= "'+ $('#newMarkerName').val() +'" localId = "'+ id +'" >'+ $('#newMarkerName').val() +'</li>')
+
+    resetNewMarkerModal()
   })
 
 })
+
+
+function resetNewMarkerModal(){
+  $("#newMarkerDescription").val('')
+  $('#newMarkerName').val('')
+  $('#img1').attr('src','images/loadImage.jpg')
+  $('#img2').attr('src','images/loadImage.jpg')
+  $('#img3').attr('src','images/loadImage.jpg')
+  $('#img4').attr('src','images/loadImage.jpg')
+  $('#pic1').val(null)
+  $('#pic2').val(null)
+  $('#pic3').val(null)
+  $('#pic4').val(null)
+}
